e96417449d4b49828e4d6c380681813416805fe7
James Warren <james.warren@helix.com>
2017-09-09 15:41:54 -0700

    add glide, extra details to model

--- app.go
+++ app.go
@@ -1,71 +1,69 @@
 package main
 
 import (
-    "database/sql"
-    "encoding/json"
-    "fmt"
-    "log"
-    "net/http"
-    "strconv"
-    "github.com/gorilla/mux"
-    _ "github.com/lib/pq"
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
+	"log"
+	"net/http"
+	"strconv"
 )
 
 type App struct {
-    Router *mux.Router
-    DB     *sql.DB
+	Router *mux.Router
+	DB     *sql.DB
 }
 
 func (a *App) Initialize(host, user, password, dbname, sslmode string) {
-    connectionString :=
-      fmt.Sprintf("host=%s user=%s password=%s dbname=%s sslmode=%s", host, user, password, dbname, sslmode)
+	connectionString :=
+		fmt.Sprintf("host=%s user=%s password=%s dbname=%s sslmode=%s", host, user, password, dbname, sslmode)
 
+	var err error
 
+	a.DB, err = sql.Open("postgres", connectionString)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    var err error
-
-    a.DB, err = sql.Open("postgres", connectionString)
-    if err != nil {
-        log.Fatal(err)
-    }
-
-    a.Router = mux.NewRouter()
-    a.initializeRoutes()
+	a.Router = mux.NewRouter()
+	a.initializeRoutes()
 }
 
 func (a *App) Run(addr string) {
-    log.Fatal(http.ListenAndServe(addr, a.Router))
+	log.Fatal(http.ListenAndServe(addr, a.Router))
 }
 
 func (a *App) initializeRoutes() {
-    a.Router.HandleFunc("/products", a.getProducts).Methods("GET")
+	a.Router.HandleFunc("/products", a.getProducts).Methods("GET")
 }
 
 func (a *App) getProducts(w http.ResponseWriter, r *http.Request) {
-    pattern := r.FormValue("pattern")
-    count, _ := strconv.Atoi(r.FormValue("count"))
+	pattern := r.FormValue("pattern")
+	count, _ := strconv.Atoi(r.FormValue("count"))
 
-    if count > 10 || count < 1 {
-        count = 10
-    }
+	if count > 10 || count < 1 {
+		count = 10
+	}
 
-    products, err := getProducts(a.DB, pattern, count)
-    if err != nil {
-        respondWithError(w, http.StatusInternalServerError, err.Error())
-        return
-    }
+	products, err := getProducts(a.DB, pattern, count)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
 
-    respondWithJSON(w, http.StatusOK, products)
+	respondWithJSON(w, http.StatusOK, products)
 }
 
 func respondWithError(w http.ResponseWriter, code int, message string) {
-    respondWithJSON(w, code, map[string]string{"error": message})
+	respondWithJSON(w, code, map[string]string{"error": message})
 }
 
 func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
-    response, _ := json.Marshal(payload)
+	response, _ := json.Marshal(payload)
 
-    w.Header().Set("Content-Type", "application/json")
-    w.WriteHeader(code)
-    w.Write(response)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
 }
--- model.go
+++ model.go
@@ -1,38 +1,37 @@
 package main
 
 import (
-    "database/sql"
-    //"log"
+	"database/sql"
 )
 
 type product struct {
-    ID    int     `json:"id"`
-    Brand  string  `json:"brand"`
-    Name string `json:"name"`
+	ID    int     `json:"id"`
+	Brand string  `json:"brand"`
+	Name  string  `json:"name"`
+	Price float32 `json:"price"`
+	Image string  `json:"image"`
 }
 
-
 func getProducts(db *sql.DB, pattern string, count int) ([]product, error) {
-    rows, err := db.Query(
-        "SELECT id, brand, name FROM products WHERE (name ILIKE '%' || $1 || '%') OR (brand ILIKE '%' || $1 || '%') LIMIT $2",
-        pattern, count)
+	rows, err := db.Query(
+		"SELECT id, brand, name, price, image_link FROM products WHERE (name ILIKE '%' || $1 || '%') OR (brand ILIKE '%' || $1 || '%') LIMIT $2",
+		pattern, count)
 
-    if err != nil {
-    //  log.Fatal("pattern=%s count=%d", pattern, count)
-        return nil, err
-    }
+	if err != nil {
+		return nil, err
+	}
 
-    defer rows.Close()
+	defer rows.Close()
 
-    products := []product{}
+	products := []product{}
 
-    for rows.Next() {
-        var p product
-        if err := rows.Scan(&p.ID, &p.Brand, &p.Name); err != nil {
-            return nil, err
-        }
-        products = append(products, p)
-    }
+	for rows.Next() {
+		var p product
+		if err := rows.Scan(&p.ID, &p.Brand, &p.Name); err != nil {
+			return nil, err
+		}
+		products = append(products, p)
+	}
 
-    return products, nil
+	return products, nil
 }
